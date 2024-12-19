c80e7aa87be21044401a878b2348f3640a087e46
Lucas Acosta <lucasmacosta@gmail.com>
2024-12-19 19:39:21 -0300

    Configure CORS

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import "express-async-errors";
 
 import redirect from "./routes/redirect";
@@ -12,6 +13,7 @@ export const app = express();
 
 // middleware for json body parsing
 app.use(bodyParser.json({ limit: "5mb" }));
+app.use(cors());
 
 app.use("/urls", urls);
 app.use("/users", users);
